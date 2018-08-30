8ac273e876f2f88f67d386b3527599adc2b07a7f
Prashant Shukla <prashantshukla@qainfotech.com>
2018-08-30 18:59:05 +0530

    Added button for atl text and table alignement

--- web-app/webapp.py
+++ web-app/webapp.py
@@ -31,13 +31,22 @@ def search_weburl():
     if "wikipedia" not in request.form['test-url']:
         flash('This demo works only on Wikipedia!!!')
         return home()
-    image_details = PageParser("chrome", request.form['test-url'])\
-        .launch_browser().get_vision_feedback()
-    if image_details.__len__() == 0:
-        flash('Either \'infobox\' or \'image in infobox\' \
-        is absent from the webpage!!!')
-        return home()
-    return render_template('resultpage.html', data=image_details)
+    if request.form['action'] == 'Get Alt Text':
+        image_details = PageParser("chrome", request.form['test-url'])\
+            .launch_browser().get_images_and_alt_text()
+        if image_details.__len__() == 0:
+            flash('Either \'infobox\' or \'image in infobox\' \
+                is absent from the webpage!!!')
+            return home()
+        return render_template('resultpage-alt.html', data=image_details)
+    if request.form['action'] == 'Test Alt Text Relevancy':
+        image_details = PageParser("chrome", request.form['test-url'])\
+            .launch_browser().get_vision_feedback()
+        if image_details.__len__() == 0:
+            flash('Either \'infobox\' or \'image in infobox\' \
+                is absent from the webpage!!!')
+            return home()
+        return render_template('resultpage.html', data=image_details)
 
 
 @app.route('/logout')
@@ -48,4 +57,4 @@ def logout():
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
