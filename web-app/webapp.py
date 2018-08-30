d8e672539bf50769c7a8954440a469021ad4fa8b
Prashant Shukla <prashantshukla@qainfotech.com>
2018-08-30 19:21:38 +0530

    Supreesed numpy warning

--- web-app/webapp.py
+++ web-app/webapp.py
@@ -57,4 +57,4 @@ def logout():
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
