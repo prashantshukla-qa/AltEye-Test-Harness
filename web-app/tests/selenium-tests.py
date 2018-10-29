a0ecb0fb7c8a87493b98a0b435190bf42463c3d6
jaspal12345 <jaspalchandramola29@gmail.com>
2018-10-29 09:45:05 +0530

    removing the unit test class from test

--- web-app/tests/selenium-tests.py
+++ web-app/tests/selenium-tests.py
@@ -1,15 +1,14 @@
 import unittest
 from selenium import webdriver
-#from accessibility_app.VicinityElements import GetVicinityText
-from ImageWebdriver import Image_Chrome_Webdriver
+from automation_tool.VicinityElements import GetVicinityText
+from automation_tool.ImageWebdriver import Image_Chrome_Webdriver
 
-class SeleniumTest(unittest.TestCase):
+class SeleniumTest():
 
     def test_Hello(self):
         image_details = {}
-        print("here1")
         driver = Image_Chrome_Webdriver()
-        driver.get("https://google.com")
+        driver.get("https://en.wikipedia.org/wiki/Car")
         image_elements = driver.find_elements_by_tag_name('img')
 
         for index, element in enumerate(image_elements):
@@ -17,9 +16,9 @@ class SeleniumTest(unittest.TestCase):
                 image_details[str(index)] = \
                     {"src": element.get_attribute("src"),
                         "alt": element.get_attribute("alt"),
-                        "vicinity_text": "" #GetVicinityText.get_vicinity_text(element,2)
+                        "vicinity_text": GetVicinityText.get_vicinity_text(element,1)
                     }
                 driver.Test_Alt_Text_Relevancy(image_details[str(index)]['src'],image_details[str(index)]['alt'],image_details[str(index)]['vicinity_text'])        
         
 if __name__ == '__main__':
-    unittest.main()
+    SeleniumTest().test_Hello()
