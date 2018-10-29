f45ddcb5333fef351ba821ddaae7c63604f1a928
jaspal12345 <jaspalchandramola29@gmail.com>
2018-10-29 09:52:50 +0530

    handling the keyErrors on failed result from google api

--- web-app/accessibility_app/Image_Detection/Image_Data_Scanner.py
+++ web-app/accessibility_app/Image_Detection/Image_Data_Scanner.py
@@ -21,20 +21,26 @@ class Image_Scanner:
         else:
             self.data_dict = self.req.get_Image_Information_from_vision_api(
                 url)
-        for data_list in self.data_dict["responses"][0]["labelAnnotations"]:
-            if data_list['description'] in "text":
-                self.is_text_present_in_Image = True
-            if data_list['score']*100 > self.match_threshhold:
-                result_set.append(
-                    {'Entity': data_list['description'],
-                     'confidence': round(data_list['score']*100, 2)})
-
-        if self.is_text_present_in_Image:
-            text = self.data_dict["responses"][0]["fullTextAnnotation"]['text']
-            text_from_Image_list = text.split("\n")
-            for eachtext in text_from_Image_list:
-                self.text_from_Image.append({'Entity': eachtext,
-                                             'confidence': self.match_threshhold})
+        try:
+            for data_list in self.data_dict["responses"][0]["labelAnnotations"]:
+                if data_list['description'] in "text":
+                    self.is_text_present_in_Image = True
+                if data_list['score']*100 > self.match_threshhold:
+                    result_set.append(
+                        {'Entity': data_list['description'],
+                        'confidence': round(data_list['score']*100, 2)})
+            
+                if self.is_text_present_in_Image:
+                    text = self.data_dict["responses"][0]["fullTextAnnotation"]['text']
+                    text_from_Image_list = text.split("\n")
+                    for eachtext in text_from_Image_list:
+                        self.text_from_Image.append({'Entity': eachtext,
+                                                    'confidence': self.match_threshhold})
+        except KeyError as error:
+             if "labelAnnotations" in str(error):
+                return [{'Entity': "None:Error Occured",'confidence': 0,}]
+             else:
+                 return result_set
 
         return result_set
 
