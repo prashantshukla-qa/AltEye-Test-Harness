0d94451d524b2b24ab05381f7695a4f0b67fe5ee
jaspal12345 <jaspalchandramola29@gmail.com>
2018-11-05 09:16:02 +0530

    method for  Saving from url and getting the base64 content

--- web-app/accessibility_app/Image_Detection/ImageSaver.py
+++ web-app/accessibility_app/Image_Detection/ImageSaver.py
@@ -1,5 +1,6 @@
 import urllib.request as urllib
 import os
+from .Encode_Image import Encode_Image
 
 class ImageSave:
 
@@ -7,4 +8,14 @@ class ImageSave:
         dir_path = os.path.dirname(os.path.realpath(__file__+"/../../"))
         urllib.urlretrieve(
             url, dir_path+"/static/images/retrieved_images/"+name)
-            
+    
+    @staticmethod 
+    def save_Image_from_url_get_encoded_content(url, name):
+        print("Saving Image offline")
+        dir_path = os.path.dirname(os.path.realpath(__file__+"/../../"))
+        urllib.urlretrieve(
+            url, dir_path+"/static/images/retrieved_images/"+name)
+        encoded_string=Encode_Image.encode_image(name)
+        #os.remove(dir_path+"/static/images/retrieved_images/"+name)
+        return encoded_string
+
