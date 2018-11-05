b748f16a98e53b5780d9fdafd730ca8cc165c69e
jaspal12345 <jaspalchandramola29@gmail.com>
2018-11-05 09:18:21 +0530

    adding vicinity text in demo

--- web-app/accessibility_app/launch_browser.py
+++ web-app/accessibility_app/launch_browser.py
@@ -27,9 +27,6 @@ class PageParser:
 
     def get_images_and_alt_text(self,width=50,height=50):
         image_details = []
-        # image_elements = self.driver\
-        #     .find_element_by_class_name('infobox')\
-        #     .find_elements_by_tag_name('img')
         image_elements = self.driver.find_elements_by_xpath('//img')
         iframes = self.driver.find_elements_by_xpath('//iframe')
         for index, iframe in enumerate(iframes):
@@ -48,48 +45,21 @@ class PageParser:
                     image_details.append(\
                         {"src": element.get_attribute("src"),
                             "alt": element.get_attribute("alt"),
-                            "vicinity_text": "",#element.find_element_by_xpath
-                            #("..").text,
+                            "vicinity_text": element.find_element_by_xpath("..").text,
                             "current_time": time.time()})
-                # request.urlretrieve(image_details[index]["src"],
-                #                     "./static/images/" +
-                #                     "retrieved_images/" +
-                #                     "image_" + str(index) + ".jpg")
-                #index += 1
         return image_details
 
-    def get_vision_feedback(self,method=1,Threshold=60,model=1,width=50,height=50):
+    def get_vision_feedback(self,model=1,Threshold=60,width=50,height=50):
         image_details = self.get_images_and_alt_text(width,height)
-        # print(image_details)
-        # for index, element in enumerate(image_details):
-        #     classes = {}
-        #     list_of_entities = Image_Scanner(80)\
-        #         .Scan_Image(image_details[index]["src"])
-        #     print(list_of_entities)
-        #     image_details[index]["alt"] = re.sub(
-        #         "\.(\w+)$", "", image_details[index]["alt"])
-        #     classesFromText = DetectText()\
-        #         .detectTextIn(image_details[index]["alt"])
-        #     classes["possible_texts"] = []
-        #     classes["text_classes"] = classesFromText
-        #     classes["result"] = False
-        #     for item in list_of_entities:
-        #         classes["possible_texts"].append(item)
-        #         if item["Entity"] in classes["text_classes"]:
-        #             classes["result"] = "GREEN"
-        #         if classes["result"] is False:
-        #             classes["result"] = "RED"
-        #     image_details[index]['classes'] = classes
-        # print(image_details)
-        metodDict={"1":"googleAPI","2":"ImageAI"}
         modelDict={"1":"DenseNet","2":"ResNet","3":"SqueezeNet","4":"InceptionV3"}
 
         for index, element in enumerate(image_details):
             classes = {}
             image_details[index]["alt"] = re.sub(
                 "\.(\w+)$", "", image_details[index]["alt"])
+            print(index)
             classes = Verify_Guidelines().ExtractClasses(image_details[index]["src"], image_details[
-                index]["alt"],image_details[index]["vicinity_text"], metodDict[str(method)],Threshold,modelDict[str(model)])
+                index]["alt"],image_details[index]["vicinity_text"],Threshold,modelDict[str(model)])
             image_details[index]['classes'] = classes
         return image_details
     def get_driver_instance(self):
