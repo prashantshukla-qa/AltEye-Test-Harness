54c4b230a50b6fd365fc65f038cfeb5b69f14045
jaspal12345 <jaspalchandramola29@gmail.com>
2018-11-05 09:17:28 +0530

    handling  empty alt text

--- web-app/accessibility_app/Verify_Guidelines.py
+++ web-app/accessibility_app/Verify_Guidelines.py
@@ -5,23 +5,20 @@ from Image_AI import Image_prediction
 
 class Verify_Guidelines:
 
-    def ExtractClasses(self, url, alt, vicinity_text, method="googleAPI", Threshold=80, model="DenseNet"):
+    def ExtractClasses(self, url, alt, vicinity_text, Threshold=80, model="DenseNet",CustomModel=None,CustomJson=None):
 
         self.detectText = DetectText.DetectText()
 
         classes = {}
-        if method in "googleAPI":
-            self.scan_Image = Image_Data_Scanner.Image_Scanner(Threshold)
-            print("using google API")
-            list_of_entities = self.scan_Image.Scan_Image(url)
-            Image_has_Text = self.scan_Image.is_Text_Present_In_Image()
-        else:
-            self.scan_Image_via_Image_AI = Image_prediction.Predict_Image(
-                Threshold, model)
-            print("using ImageAI API")
-            list_of_entities = self.scan_Image_via_Image_AI.get_classes_from_image(
-                url)
-            Image_has_Text = False
+        self.scan_Image = Image_Data_Scanner.Image_Scanner(Threshold)
+        print("using google API")
+        list_of_entities = self.scan_Image.Scan_Image(url)
+        Image_has_Text = self.scan_Image.is_Text_Present_In_Image()
+        self.scan_Image_via_Image_AI = Image_prediction.Predict_Image(Threshold, model,CustomModel,CustomJson)
+        print("Now using ImageAI API")
+        list_of_entities=list_of_entities +self.scan_Image_via_Image_AI.get_classes_from_image(url)
+        
+        Image_has_Text = False
         classes["possible_texts"] = []
         classes["result"] = False
         if Image_has_Text:
@@ -35,6 +32,20 @@ class Verify_Guidelines:
                         if text["Entity"].lower() in exact_text:
                             classes["result"] = "GREEN"
         classesFromText = self.detectText.detectTextIn(alt)
+        classesFromVicinityText=self.detectText.detectTextIn(vicinity_text)
+        
+        result_check_for_vicinity="RED"
+        if not alt:
+            classes["text_classes"] = classesFromVicinityText
+            for item in list_of_entities:
+                classes["possible_texts"].append(item)
+                if item["Entity"] in classes["text_classes"]:
+                   result_check_for_vicinity = "GREEN"      
+            if result_check_for_vicinity == "GREEN":
+                classes["result"] = "RED:: alt text was not found but it seems content if picture has relevance with vicinity text"
+            else:
+                classes["result"] = "GREEN:: alt text was not found and it seems content of picture has no relvance with vicinity text"
+
         classes["text_classes"] = classesFromText
         for item in list_of_entities:
             classes["possible_texts"].append(item)
